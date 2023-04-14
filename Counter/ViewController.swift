024d56d6c8a95b04601d3ca0ddf76c0f88d8f871
frol182 <frol182@gmail.com>
2023-04-14 17:19:23 +0300

    Версия 0.1

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Переменные
+    var counterValue: Int = 0  // Числовая переменная для подсчета
+    @IBOutlet weak var counterLabel: UILabel! // Лейбл для вывода счетчика
+    @IBOutlet weak var logTextField: UITextView! // Окно для вывода лога
+    
+    //Кнопка для увеличения счета
+    @IBAction func counterButtoPlus(_ sender: Any) {
+    counterValue += 1
+    counterLabel.text = "Значение счетчика " + counterValue.formatted() // Вывод счетчика
+    logTextField.text +=   // Вывод лога
+        "\(Date().formatted(date: .omitted, time: .shortened))  "
+        + "Значение увеличено"
+        + "\n"
+    }
+    
+    //Кнопка для уменьшения счета
+    @IBAction func counterButtonMinus(_ sender: Any) {
+        if counterValue > 0 {
+            counterValue -= 1
+            counterLabel.text = "Значение счетчика " + counterValue.formatted()
+            logTextField.text +=
+            "\(Date().formatted(date: .omitted, time: .shortened))  "
+            + "Значение уменьшено"
+            + "\n"
+        }
+        else {logTextField.text +=
+            "\(Date().formatted(date: .omitted, time: .shortened))  "
+            + "Попытка уменьшить значение счётчика ниже 0"
+            + "\n"
+        }
+    }
+    
+    //Кнопка для сброса счета
+    @IBAction func counterButtonReset(_ sender: Any) {
+        counterValue = 0
+        counterLabel.text = "Значение счетчика " + counterValue.formatted() // Вывод счетчика
+        logTextField.text +=   // Вывод лога
+            "\(Date().formatted(date: .omitted, time: .shortened))  "
+            + "Значение сброшено"
+            + "\n"
+    }
+    
+    //Инициализация значений при запуске
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterValue = 0 // обнуление числовой переменной при загрузке экрана
+        counterLabel.text = "Значение счетчика " + counterValue.formatted()
+        logTextField.text = ""
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
